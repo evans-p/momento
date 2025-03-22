085edb643bf364b5e5f522a9fe906eba37440af3
poulakise <evanspoulaki@gmail.com>
2025-03-22 03:00:11 +0200

    feat(project): initial project structure

---
+++ src/test/java/gr/evansp/momento/MomentoApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.evansp.momento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
