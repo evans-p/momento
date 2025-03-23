06f792f2e350261dac7b555e8d2ab3a87ee512b9
poulakise <evanspoulaki@gmail.com>
2025-03-23 02:32:13 +0200

    fix(AssetRepository): cleanup.

--- cdn/src/main/java/gr/evansp/momento/AssetRepository.java
+++ cdn/src/main/java/gr/evansp/momento/AssetRepository.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, UUID> {
+
 	/**
 	 * Find asset by contentHash.
 	 *
