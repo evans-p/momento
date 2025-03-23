d1c4b13290c9f0c9b82c8ccc4fc585ad44f16c15
poulakise <evanspoulaki@gmail.com>
2025-03-23 02:28:32 +0200

    feat(AssetRepository): added repository for Asset: AssetRepository.

---
+++ cdn/src/main/java/gr/evansp/momento/AssetRepository.java
@@ -0,0 +1,32 @@
+package gr.evansp.momento;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import gr.evansp.momento.model.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for {@link Asset}.
+ */
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, UUID> {
+	/**
+	 * Find asset by contentHash.
+	 *
+	 * @param contentHash contentHash
+	 *
+	 * @return Asset
+	 */
+	Optional<Asset> findByContentHash(String contentHash);
+
+	/**
+	 * Find asset by path.
+	 *
+	 * @param path path
+	 *
+	 * @return Asset
+	 */
+	Optional<Asset> findByPath(String path);
+}
