1fb09c15eaf8fc4498c782b1f990c5d8a77ae019
poulakise <evanspoulaki@gmail.com>
2025-03-24 17:03:28 +0200

    docs(ConstraintViolationExceptionMessage): javadoc for ConstraintViolationExceptionMessage.

--- cdn/src/main/java/gr/evansp/momento/bean/ConstraintViolationExceptionMessage.java
+++ cdn/src/main/java/gr/evansp/momento/bean/ConstraintViolationExceptionMessage.java
@@ -2,5 +2,12 @@ package gr.evansp.momento.bean;
 
 import java.util.Map;
 
+import jakarta.validation.ConstraintViolationException;
+
+/**
+ * Bean record to be returned on the event of {@link ConstraintViolationException}.
+ *
+ * @param messages messages to be returned.
+ */
 public record ConstraintViolationExceptionMessage(Map<String, String> messages) {
 }
