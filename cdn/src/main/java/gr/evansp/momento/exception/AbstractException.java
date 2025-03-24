0982fc5163d187b2ca33217519be33963d40e2ea
poulakise <evanspoulaki@gmail.com>
2025-03-24 17:07:47 +0200

    docs(AbstractException): javadoc for AbstractException.

--- cdn/src/main/java/gr/evansp/momento/exception/AbstractException.java
+++ cdn/src/main/java/gr/evansp/momento/exception/AbstractException.java
@@ -3,6 +3,9 @@ package gr.evansp.momento.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Abstract Exception. To be extended by all application Exceptions.
+ */
 @Getter
 @AllArgsConstructor
 abstract class AbstractException extends Exception {
