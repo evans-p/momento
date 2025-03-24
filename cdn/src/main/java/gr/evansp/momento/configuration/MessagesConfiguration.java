31d4921d8de94296eddf405d0015934967e6ec19
poulakise <evanspoulaki@gmail.com>
2025-03-24 17:06:48 +0200

    docs(MessagesConfiguration): javadoc for MessagesConfiguration.

--- cdn/src/main/java/gr/evansp/momento/configuration/MessagesConfiguration.java
+++ cdn/src/main/java/gr/evansp/momento/configuration/MessagesConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+/**
+ * {@link Configuration} for {@link MessageSource}.
+ */
 @Configuration
 public class MessagesConfiguration {
 	/**
