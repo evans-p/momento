5e5925c5832618f7c134c94a95d4487d07579eb1
poulakise <evanspoulaki@gmail.com>
2025-03-29 01:31:14 +0200

    feat(AbstractUnitTest): further enhancements on AbstractUnitTest.

--- cdn/src/test/java/gr/evansp/momento/AbstractUnitTest.java
+++ cdn/src/test/java/gr/evansp/momento/AbstractUnitTest.java
@@ -1,7 +1,14 @@
 package gr.evansp.momento;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Base Class for Unit tests.
@@ -18,4 +25,17 @@ public class AbstractUnitTest {
 	 */
 	protected static final ResourceBundle VALIDATION_MESSAGES =
 			ResourceBundle.getBundle("ValidationMessages", Locale.getDefault());
+
+	protected static final byte[] PNG_IMAGE = loadImage("images/sample.png");
+	protected static final byte[] JPG_IMAGE = loadImage("images/sample.jpeg");
+
+	private static byte[] loadImage(String imagePath) {
+		ClassPathResource resource = new ClassPathResource(imagePath);
+
+		try {
+			return resource.getContentAsByteArray();
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
