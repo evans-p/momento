d98212f50407779bbc5c53db8e4207ab531ceb90
poulakise <evanspoulaki@gmail.com>
2025-03-30 07:13:31 +0300

    test(AssetService): added integration tests for AssetService.

---
+++ cdn/src/test/java/gr/evansp/momento/service/TestAssetServiceIT.java
@@ -0,0 +1,37 @@
+package gr.evansp.momento.service;
+
+
+import gr.evansp.momento.AbstractIntegrationTest;
+import gr.evansp.momento.model.Asset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.multipart.MultipartFile;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Integration tests for {@link AssetService}.
+ */
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class TestAssetServiceIT extends AbstractIntegrationTest {
+
+	@Autowired
+	AssetService service;
+
+	@Test
+	public void testUploadAsset() {
+		MultipartFile file = new MockMultipartFile("file", "Yosuke.jpg", "image/jpeg", JPG_IMAGE);
+
+		Asset asset = service.uploadAsset(file);
+
+		assertNotNull(asset);
+		assertNotNull(asset.getId());
+	}
+}
