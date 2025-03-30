4a6b16baf775a8eebf6bf981a90e9f552aa3ca59
poulakise <evanspoulaki@gmail.com>
2025-03-30 08:56:28 +0300

    feat(FileNameValidator): new validator, FileNameValidator.

---
+++ cdn/src/main/java/gr/evansp/momento/annotation/ValidFileName.java
@@ -0,0 +1,25 @@
+package gr.evansp.momento.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import gr.evansp.momento.validator.FileNameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Annotation to validate {@link MultipartFile}.
+ */
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FileNameValidator.class)
+@Documented
+public @interface ValidFileName {
+	String message() default "{faulty.file.name}";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
---
+++ cdn/src/main/java/gr/evansp/momento/validator/FileNameValidator.java
@@ -0,0 +1,54 @@
+package gr.evansp.momento.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import gr.evansp.momento.annotation.ValidFileName;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class FileNameValidator implements ConstraintValidator<ValidFileName, String> {
+
+	private static final String REGEX = "^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}-[a-zA-Z0-9]{8}\\.(jpg|jpeg|png)$";
+
+	/**
+	 * isValid
+	 */
+	private boolean isValid = true;
+
+	@Override
+	public void initialize(ValidFileName constraintAnnotation) {
+		ConstraintValidator.super.initialize(constraintAnnotation);
+	}
+
+	@Override
+	public boolean isValid(String name, ConstraintValidatorContext context) {
+
+		if (name == null || name.isBlank()) {
+			isValid = buildConstraintViolationMessage("{faulty.file.name}", context);
+		} else {
+			Pattern pattern = Pattern.compile(REGEX);
+			Matcher matcher = pattern.matcher(name);
+			if (!matcher.matches()) {
+				isValid = buildConstraintViolationMessage("{faulty.file.name}", context);
+			}
+		}
+		return isValid;
+	}
+
+	/**
+	 * Build validation message.
+	 *
+	 * @param messageCode
+	 * 		messageCode
+	 * @param context
+	 * 		context
+	 * @return false
+	 */
+	private boolean buildConstraintViolationMessage(String messageCode, ConstraintValidatorContext context) {
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(messageCode).addConstraintViolation();
+
+		return false;
+	}
+}
