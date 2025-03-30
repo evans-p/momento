27dd34f63ee1572ced42dd830dd82f428bdb39de
poulakise <evanspoulaki@gmail.com>
2025-03-30 06:46:10 +0300

    feat(ExceptionMessage): new record bean, ExceptionMessage

---
+++ cdn/src/main/java/gr/evansp/momento/bean/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package gr.evansp.momento.bean;
+
+/**
+ * record to be returned when an {@link Exception} occurs.
+ * @param message
+ */
+public record ExceptionMessage(String message) {
+	//EMPTY
+}
