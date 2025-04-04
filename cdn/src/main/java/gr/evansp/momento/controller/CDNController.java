345ebb952f7aec9937bf1a1a2e50ebceda9f3530
poulakise <evanspoulaki@gmail.com>
2025-04-05 01:28:40 +0300

    feat(CDNController): new controller: CDNController.

---
+++ cdn/src/main/java/gr/evansp/momento/controller/CDNController.java
@@ -0,0 +1,41 @@
+package gr.evansp.momento.controller;
+
+
+import gr.evansp.momento.dto.AssetDto;
+import gr.evansp.momento.model.Asset;
+import gr.evansp.momento.service.AssetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("cdn/v1/assets/")
+public class CDNController {
+
+	private final AssetService service;
+
+	@Autowired
+	public CDNController(AssetService service) {
+		this.service = service;
+	}
+
+	@PostMapping("upload")
+	public ResponseEntity<AssetDto> upload(@RequestParam("file") MultipartFile file) {
+		Asset asset = service.uploadAsset(file);
+		return new ResponseEntity<>(AssetDto.of(asset), HttpStatus.OK);
+	}
+
+	@GetMapping("{fileName}")
+	public ResponseEntity<Resource> getFile(@PathVariable String fileName) {
+		return new ResponseEntity<>(new FileSystemResource(service.getFileByName(fileName)), HttpStatus.OK);
+	}
+}
