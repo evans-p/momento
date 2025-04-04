687ca863c4b3af6d0ebe8c2d10fa06836a46c173
poulakise <evanspoulaki@gmail.com>
2025-04-05 01:21:10 +0300

    feat(AssetDto): new record AssetDto

---
+++ cdn/src/main/java/gr/evansp/momento/dto/AssetDto.java
@@ -0,0 +1,33 @@
+package gr.evansp.momento.dto;
+
+import java.time.LocalDateTime;
+
+import gr.evansp.momento.model.Asset;
+
+/**
+ * Dto for {@link Asset}.
+ *
+ * @param fileName
+ * 		fileName
+ * @param contentType
+ * 		contentType
+ * @param fileSize
+ * 		fileSize
+ * @param uploadDate
+ * 		uploadDate
+ */
+public record AssetDto(String fileName, String contentType, Long fileSize, LocalDateTime uploadDate) {
+
+
+	/**
+	 * Static transformation method from {@link Asset} to {@link AssetDto}.
+	 *
+	 * @param asset
+	 *        {@link Asset}.
+	 * @return {@link AssetDto}.
+	 */
+	public static AssetDto of(Asset asset) {
+		return new AssetDto(asset.getFileName(), asset.getContentType(), asset.getFileSize(),
+				asset.getUploadDate().toLocalDateTime());
+	}
+}
