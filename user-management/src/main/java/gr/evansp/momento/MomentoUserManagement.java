539cc250f8808653b2fc4afd34953092b0abee19
poulakise <evanspoulaki@gmail.com>
2025-04-12 05:12:45 +0300

    feat(MomentoUserManagement): added main class for MomentoUserManagement.

---
+++ user-management/src/main/java/gr/evansp/momento/MomentoUserManagement.java
@@ -0,0 +1,11 @@
+package gr.evansp.momento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentoUserManagement {
+	public static void main(String[] args) {
+		SpringApplication.run(MomentoUserManagement.class, args);
+	}
+}
