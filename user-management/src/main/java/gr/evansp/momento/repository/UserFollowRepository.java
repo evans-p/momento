7ade79248f1c3fd11d4ebf681f99abb5106ffdfb
poulakise <evanspoulaki@gmail.com>
2025-04-12 06:26:45 +0300

    feat(UserFollowRepository): new method on UserFollowRepository: findByFollowerAndFollowed

--- user-management/src/main/java/gr/evansp/momento/repository/UserFollowRepository.java
+++ user-management/src/main/java/gr/evansp/momento/repository/UserFollowRepository.java
@@ -2,6 +2,8 @@ package gr.evansp.momento.repository;
 
 import gr.evansp.momento.model.UserFollow;
 import gr.evansp.momento.model.UserProfile;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,4 +16,6 @@ public interface UserFollowRepository extends JpaRepository<UserFollow, UUID> {
   Page<UserFollow> findByFollower(UserProfile follower, Pageable pageable);
 
   Page<UserFollow> findByFollowed(UserProfile followed, Pageable pageable);
+
+  Optional<UserFollow> findByFollowerAndFollowed(UserProfile follower, UserProfile followed);
 }
