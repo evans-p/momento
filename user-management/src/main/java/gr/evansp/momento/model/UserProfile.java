918229d5939504e373201825bcd90fa5fc2c3b15
poulakise <evanspoulaki@gmail.com>
2025-04-12 05:15:07 +0300

    feat(UserProfile): Updated UserProfile to include UserFollow.

--- user-management/src/main/java/gr/evansp/momento/model/UserProfile.java
+++ user-management/src/main/java/gr/evansp/momento/model/UserProfile.java
@@ -1,14 +1,19 @@
 package gr.evansp.momento.model;
 
 import java.time.OffsetDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,16 +39,28 @@ public class UserProfile {
 	private String email;
 
 	@Column(name = "CREATED_AT", nullable = false)
-	private OffsetDateTime createdAt;
+	private OffsetDateTime createdAt = OffsetDateTime.now();
 
 	@Column(name = "UPDATED_AT", nullable = false)
-	private OffsetDateTime updatedAt;
+	private OffsetDateTime updatedAt = OffsetDateTime.now();;
 
 	@Column(name = "PROFILE_PICTURE_URL")
 	private String profilePictureUrl;
 
-	@Column(name = "AUTHENTICATION_PROVIDER_ID", nullable = false)
+	@Column(name = "AUTHENTICATION_PROVIDER_ID", nullable = false, unique = true)
 	private String authenticationProviderId;
+
+	@Column(name = "FOLLOWERS_COUNT", nullable = false, unique = true)
+	private Long followersCount = 0L;
+
+	@Column(name = "FOLLOWED_COUNT", nullable = false, unique = true)
+	private Long followedCount = 0L;
+
+	@OneToMany(mappedBy = "followed", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<UserFollow> followers = new HashSet<>();
+
+	@OneToMany(mappedBy = "follower", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<UserFollow> followed = new HashSet<>();
 
 	@Override
 	public boolean equals(Object o) {
