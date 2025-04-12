529f97918a6e2e1c4db8a28d968d14cafe0c0376
poulakise <evanspoulaki@gmail.com>
2025-04-12 05:14:40 +0300

    feat(UserFollow): new class: UserFollow.

---
+++ user-management/src/main/java/gr/evansp/momento/model/UserFollow.java
@@ -0,0 +1,53 @@
+package gr.evansp.momento.model;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(schema = "USER_MANAGEMENT", name = "USER_FOLLOW")
+public class UserFollow {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "ID", nullable = false, unique = true)
+	UUID id;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "FOLLOWER_ID")
+	UserProfile follower;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "FOLLOWED_ID")
+	UserProfile followed;
+
+	@Column(name = "CREATED_AT", nullable = false)
+	private OffsetDateTime createdAt = OffsetDateTime.now();
+
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+
+		UserFollow userFollow = (UserFollow) o;
+		return id.equals(userFollow.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+
+}
---
+++ user-management/src/test/java/gr/evansp/momento/AbstractIntegrationTest.java
@@ -0,0 +1,35 @@
+package gr.evansp.momento;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+/**
+ * Abstract Class for Integration tests. Contains the initialization of
+ * the required test containers.
+ */
+public abstract class AbstractIntegrationTest {
+
+  private static final PostgreSQLContainer<?> postgres =
+      new PostgreSQLContainer<>("postgres:14")
+          .withDatabaseName("user-management")
+          .withUsername("postgres")
+          .withPassword("postgres")
+          .withExposedPorts(5432)
+          .withInitScript("sql/ddl.sql")
+          .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 2));
+
+  static {
+    postgres.start();
+  }
+
+  @DynamicPropertySource
+  private static void registerPgProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add(
+        "spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
+  }
+}
