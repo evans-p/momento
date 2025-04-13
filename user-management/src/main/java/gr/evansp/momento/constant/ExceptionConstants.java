90d47f237ec8cdbd46c83ea191b0147cf0b45784
poulakise <evanspoulaki@gmail.com>
2025-04-14 02:34:18 +0300

    feat(ExceptionConstants): updated ExceptionConstants.

--- user-management/src/main/java/gr/evansp/momento/constant/ExceptionConstants.java
+++ user-management/src/main/java/gr/evansp/momento/constant/ExceptionConstants.java
@@ -15,6 +15,16 @@ public class ExceptionConstants {
   public static final String USER_ALREADY_REGISTERED = "user.already.registered";
 
   /**
+   * INVALID USER ID.
+   */
+  public static final String INVALID_USER_ID = "invalid.user.id";
+
+  /**
+   * INVALID USER ID.
+   */
+  public static final String USER_NOT_FOUND = "user.not.found";
+
+  /**
    * Private NoArgs Constructor.
    */
   private ExceptionConstants() {
