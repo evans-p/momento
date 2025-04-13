edcac46dfed83a8672c3e7bc8b656cffea1ff954
poulakise <evanspoulaki@gmail.com>
2025-04-13 23:20:11 +0300

    chore(AbstractUnitTest): spotless.

--- user-management/src/test/java/gr/evansp/momento/AbstractUnitTest.java
+++ user-management/src/test/java/gr/evansp/momento/AbstractUnitTest.java
@@ -2,20 +2,18 @@ package gr.evansp.momento;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-
 import org.springframework.core.io.ClassPathResource;
 
 public class AbstractUnitTest {
 
-	protected static final String VALID_TOKEN = loadValidToken();
+  protected static final String VALID_TOKEN = loadValidToken();
 
-
-	private static String loadValidToken() {
-		ClassPathResource resource = new ClassPathResource("util/jwt-token.txt");
-		try {
-			return resource.getContentAsString(Charset.defaultCharset());
-		} catch (IOException e) {
-			return null;
-		}
-	}
+  private static String loadValidToken() {
+    ClassPathResource resource = new ClassPathResource("util/jwt-token.txt");
+    try {
+      return resource.getContentAsString(Charset.defaultCharset());
+    } catch (IOException e) {
+      return null;
+    }
+  }
 }
