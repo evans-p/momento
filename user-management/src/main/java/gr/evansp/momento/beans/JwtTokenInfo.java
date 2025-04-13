dfec275bc08c58b5779566eb8b2db95993cb1379
poulakise <evanspoulaki@gmail.com>
2025-04-13 22:36:23 +0300

    feat(JwtTokenInfo): new class JwtTokenInfo.

---
+++ user-management/src/main/java/gr/evansp/momento/beans/JwtTokenInfo.java
@@ -0,0 +1,21 @@
+package gr.evansp.momento.beans;
+
+import com.auth0.jwt.JWT;
+
+/**
+ * The extracted info from a {@link JWT} token.
+ *
+ * @param authenticationProviderId
+ * 		authenticationProviderId
+ * @param firstName
+ * 		firstName
+ * @param lastName
+ * 		lastName
+ * @param email
+ * 		email
+ * @param profilePictureUrl
+ * 		profilePictureUrl
+ */
+public record JwtTokenInfo(String authenticationProviderId, String firstName, String lastName, String email,
+                           String profilePictureUrl) {
+}
