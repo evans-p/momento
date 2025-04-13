4057d811f7cc85e8a698a5485efb5bc022f367a9
poulakise <evanspoulaki@gmail.com>
2025-04-13 22:34:43 +0300

    feat(ExceptionConstants): new class ExceptionConstants

---
+++ user-management/src/main/java/gr/evansp/momento/constant/ExceptionConstants.java
@@ -0,0 +1,18 @@
+package gr.evansp.momento.constant;
+
+/**
+ * Message code for {@link Exception}.
+ */
+public class ExceptionConstants {
+  /**
+   * FILE NOT FOUND.
+   */
+  public static final String INVALID_TOKEN = "invalid.token";
+
+  /**
+   * Private NoArgs Constructor.
+   */
+  private ExceptionConstants() {
+    // EMPTY
+  }
+}
