bc25c4786141e6b4bcba075133980aec39ec2e53
poulakise <evanspoulaki@gmail.com>
2025-04-14 02:39:59 +0300

    feat(UserManagementServiceImpl): implementation of getUser

--- user-management/src/main/java/gr/evansp/momento/service/UserManagementServiceImpl.java
+++ user-management/src/main/java/gr/evansp/momento/service/UserManagementServiceImpl.java
@@ -1,6 +1,9 @@
 package gr.evansp.momento.service;
 
 import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import gr.evansp.momento.beans.JwtTokenInfo;
 import gr.evansp.momento.exception.LogicException;
@@ -10,10 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static gr.evansp.momento.constant.ExceptionConstants.INVALID_USER_ID;
 import static gr.evansp.momento.constant.ExceptionConstants.USER_ALREADY_REGISTERED;
+import static gr.evansp.momento.constant.ExceptionConstants.USER_NOT_FOUND;
 
 @Service
 public class UserManagementServiceImpl implements UserManagementService {
+
+	private static final String USER_ID_REGEX = "^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$";
+
 
 	@Autowired
 	JwtService jwtService;
@@ -33,6 +41,25 @@ public class UserManagementServiceImpl implements UserManagementService {
 		}
 
 		return repository.save(createUserProfileFromJwtTokenInfo(tokenInfo));
+	}
+
+	@Override
+	public UserProfile getUser(String userId) {
+		validateUserId(userId);
+
+		return repository.findById(UUID.fromString(userId)).orElseThrow(() -> new LogicException(USER_NOT_FOUND, null));
+	}
+
+	private void validateUserId(String userId) {
+		if (userId == null || userId.isBlank()) {
+			throw new LogicException(INVALID_USER_ID, new Object[]{userId});
+		}
+		Pattern pattern = Pattern.compile(USER_ID_REGEX);
+		Matcher matcher = pattern.matcher(userId);
+
+		if (!matcher.matches()) {
+			throw new LogicException(INVALID_USER_ID, new Object[]{userId});
+		}
 	}
 
 	private UserProfile createUserProfileFromJwtTokenInfo(JwtTokenInfo tokenInfo) {
