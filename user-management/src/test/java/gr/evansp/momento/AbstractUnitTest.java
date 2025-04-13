bd4286bfc8a1668079c5f3ef6e3300d25507d82c
poulakise <evanspoulaki@gmail.com>
2025-04-14 00:22:19 +0300

    feat(AbstractUnitTest): finalized AbstractUnitTest.

--- user-management/src/test/java/gr/evansp/momento/AbstractUnitTest.java
+++ user-management/src/test/java/gr/evansp/momento/AbstractUnitTest.java
@@ -6,10 +6,18 @@ import org.springframework.core.io.ClassPathResource;
 
 public class AbstractUnitTest {
 
-  protected static final String VALID_TOKEN = loadValidToken();
+  protected static final String VALID_GOOGLE_TOKEN = loadValidToken("util/google-jwt-token.txt");
 
-  private static String loadValidToken() {
-    ClassPathResource resource = new ClassPathResource("util/jwt-token.txt");
+  protected static final String VALID_KEYCLOAK_TOKEN = loadValidToken("util/keycloak-jwt-token.txt");
+
+  protected static final String VALID_FACEBOOK_TOKEN = loadValidToken("util/facebook-jwt-token.txt");
+
+  protected static final String VALID_MICROSOFT_TOKEN = loadValidToken("util/microsoft-jwt-token.txt");
+
+  protected static final String VALID_LINKED_IN_TOKEN = loadValidToken("util/linkedin-jwt-token.txt");
+
+  private static String loadValidToken(String path) {
+    ClassPathResource resource = new ClassPathResource(path);
     try {
       return resource.getContentAsString(Charset.defaultCharset());
     } catch (IOException e) {
