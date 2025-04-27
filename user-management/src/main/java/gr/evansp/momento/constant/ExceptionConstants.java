237d0e070bc0d22543622278f588900b97cb8552
poulakise <evanspoulaki@gmail.com>
2025-04-27 18:52:03 +0300

    chore(ExceptionConstants): cleanup on ExceptionConstants.

--- user-management/src/main/java/gr/evansp/momento/constant/ExceptionConstants.java
+++ user-management/src/main/java/gr/evansp/momento/constant/ExceptionConstants.java
@@ -15,11 +15,6 @@ public class ExceptionConstants {
   public static final String USER_ALREADY_REGISTERED = "user.already.registered";
 
   /**
-   * INVALID USER ID.
-   */
-  public static final String INVALID_USER_ID = "invalid.user.id";
-
-  /**
    * USER NOT FOUND.
    */
   public static final String USER_NOT_FOUND = "user.not.found";
