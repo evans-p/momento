fdd699514a29c92ff0f8bdcf03cf1ccf6c27b8b2
poulakise <evanspoulaki@gmail.com>
2025-04-27 19:07:45 +0300

    chore(TestUserIdValidator): TestUserIdValidator spotless.

--- user-management/src/test/java/gr/evansp/momento/validator/TestUserIdValidator.java
+++ user-management/src/test/java/gr/evansp/momento/validator/TestUserIdValidator.java
@@ -1,6 +1,6 @@
 package gr.evansp.momento.validator;
 
-import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import gr.evansp.momento.AbstractUnitTest;
 import gr.evansp.momento.annotation.ValidUserId;
@@ -8,106 +8,101 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit tests for {@link UserIdValidator}.
  */
 class TestUserIdValidator extends AbstractUnitTest {
 
+  @Test
+  public void testIsValid_nullUserId() {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
 
-	@Test
-	public void testIsValid_nullUserId() {
-		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-			Validator validator = factory.getValidator();
+      UserIdWrapper wrapper = new UserIdWrapper(null);
 
-			UserIdWrapper wrapper = new UserIdWrapper(null);
+      Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
 
-			Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
+      assertEquals(1, violations.size());
+      assertEquals(
+          VALIDATION_MESSAGES.getString("invalid.user.id"),
+          violations.iterator().next().getMessage());
+    }
+  }
 
-			assertEquals(1, violations.size());
-			assertEquals(
-					VALIDATION_MESSAGES.getString("invalid.user.id"),
-					violations.iterator().next().getMessage());
-		}
-	}
+  @Test
+  public void testIsValid_emptyUserId() {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
 
-	@Test
-	public void testIsValid_emptyUserId() {
-		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-			Validator validator = factory.getValidator();
+      UserIdWrapper wrapper = new UserIdWrapper("");
 
-			UserIdWrapper wrapper = new UserIdWrapper("");
+      Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
 
-			Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
+      assertEquals(1, violations.size());
+      assertEquals(
+          VALIDATION_MESSAGES.getString("invalid.user.id"),
+          violations.iterator().next().getMessage());
+    }
+  }
 
-			assertEquals(1, violations.size());
-			assertEquals(
-					VALIDATION_MESSAGES.getString("invalid.user.id"),
-					violations.iterator().next().getMessage());
-		}
-	}
+  @Test
+  public void testIsValid_blankUserId() {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
 
+      UserIdWrapper wrapper = new UserIdWrapper("    ");
 
-	@Test
-	public void testIsValid_blankUserId() {
-		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-			Validator validator = factory.getValidator();
+      Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
 
-			UserIdWrapper wrapper = new UserIdWrapper("    ");
+      assertEquals(1, violations.size());
+      assertEquals(
+          VALIDATION_MESSAGES.getString("invalid.user.id"),
+          violations.iterator().next().getMessage());
+    }
+  }
 
-			Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
+  @Test
+  public void testIsValid_faultyUserId() {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
 
-			assertEquals(1, violations.size());
-			assertEquals(
-					VALIDATION_MESSAGES.getString("invalid.user.id"),
-					violations.iterator().next().getMessage());
-		}
-	}
+      UserIdWrapper wrapper = new UserIdWrapper("Mullets");
 
-	@Test
-	public void testIsValid_faultyUserId() {
-		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-			Validator validator = factory.getValidator();
+      Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
 
-			UserIdWrapper wrapper = new UserIdWrapper("Mullets");
+      assertEquals(1, violations.size());
+      assertEquals(
+          VALIDATION_MESSAGES.getString("invalid.user.id"),
+          violations.iterator().next().getMessage());
+    }
+  }
 
-			Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
+  @Test
+  public void testIsValid_ok() {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
 
-			assertEquals(1, violations.size());
-			assertEquals(
-					VALIDATION_MESSAGES.getString("invalid.user.id"),
-					violations.iterator().next().getMessage());
-		}
-	}
+      UserIdWrapper wrapper = new UserIdWrapper("6de22d6a-60ea-4fe2-b4a4-eb1ed5fb838b");
 
+      Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
 
-	@Test
-	public void testIsValid_ok() {
-		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-			Validator validator = factory.getValidator();
+      assertEquals(0, violations.size());
+    }
+  }
 
-			UserIdWrapper wrapper = new UserIdWrapper("6de22d6a-60ea-4fe2-b4a4-eb1ed5fb838b");
+  @Setter
+  @Getter
+  private static class UserIdWrapper {
 
-			Set<ConstraintViolation<UserIdWrapper>> violations = validator.validate(wrapper);
+    @ValidUserId private String userId;
 
-			assertEquals(0, violations.size());
-		}
-	}
-
-
-	@Setter
-	@Getter
-	private static class UserIdWrapper {
-
-		@ValidUserId private String userId;
-
-		public UserIdWrapper(String userId) {
-			this.userId = userId;
-		}
-	}
+    public UserIdWrapper(String userId) {
+      this.userId = userId;
+    }
+  }
 }
