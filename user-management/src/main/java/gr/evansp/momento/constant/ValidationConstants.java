9a173b7088c06815752d27e0d7da3f57d62877c4
poulakise <evanspoulaki@gmail.com>
2025-04-27 19:08:07 +0300

    feat(ValidationConstants): new constant messages

--- user-management/src/main/java/gr/evansp/momento/constant/ValidationConstants.java
+++ user-management/src/main/java/gr/evansp/momento/constant/ValidationConstants.java
@@ -8,9 +8,19 @@ import jakarta.validation.ConstraintViolationException;
 public class ValidationConstants {
 
   /**
-   * INVALID_FILE.
+   * INVALID USER ID.
    */
   public static final String INVALID_USER_ID = "{invalid.user.id}";
+
+  /**
+   * INVALID PAGE.
+   */
+  public static final String INVALID_PAGE = "{invalid.page}";
+
+  /**
+   * INVALID PAGING.
+   */
+  public static final String INVALID_PAGING = "{invalid.paging}";
 
   /**
    * Private NoArgs Constructor.
