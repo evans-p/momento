faea081939ff2b23c73d856a7668f34aad9fa88f
poulakise <evanspoulaki@gmail.com>
2025-04-27 19:25:14 +0300

    test(PagingValidator): tests for PagingValidator.

---
+++ user-management/src/test/java/gr/evansp/momento/validator/TestPagingValidator.java
@@ -0,0 +1,110 @@
+package gr.evansp.momento.validator;
+
+import java.util.Set;
+
+import gr.evansp.momento.AbstractUnitTest;
+import gr.evansp.momento.annotation.ValidPaging;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests for {@link PagingValidator}.
+ */
+public class TestPagingValidator extends AbstractUnitTest {
+
+	/**
+	 * Test for {@link PagingValidator#isValid(Integer, ConstraintValidatorContext)}.
+	 */
+	@Test
+	public void testIsValid_nullPaging() {
+		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+			Validator validator = factory.getValidator();
+
+			PagingWrapper wrapper = new PagingWrapper(null);
+
+			Set<ConstraintViolation<PagingWrapper>> violations = validator.validate(wrapper);
+
+			assertEquals(1, violations.size());
+
+			assertEquals(
+					VALIDATION_MESSAGES.getString("invalid.paging"),
+					violations.iterator().next().getMessage());
+		}
+	}
+
+	/**
+	 * Test for {@link PagingValidator#isValid(Integer, ConstraintValidatorContext)}.
+	 */
+	@Test
+	public void testIsValid_negativePaging() {
+		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+			Validator validator = factory.getValidator();
+
+			PagingWrapper wrapper = new PagingWrapper(-1);
+
+			Set<ConstraintViolation<PagingWrapper>> violations = validator.validate(wrapper);
+
+			assertEquals(1, violations.size());
+
+			assertEquals(
+					VALIDATION_MESSAGES.getString("invalid.paging"),
+					violations.iterator().next().getMessage());
+		}
+	}
+
+	/**
+	 * Test for {@link PagingValidator#isValid(Integer, ConstraintValidatorContext)}.
+	 */
+	@Test
+	public void testIsValid_zeroPaging() {
+		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+			Validator validator = factory.getValidator();
+
+			PagingWrapper wrapper = new PagingWrapper(0);
+
+			Set<ConstraintViolation<PagingWrapper>> violations = validator.validate(wrapper);
+
+			assertEquals(1, violations.size());
+
+			assertEquals(
+					VALIDATION_MESSAGES.getString("invalid.paging"),
+					violations.iterator().next().getMessage());
+		}
+	}
+
+	/**
+	 * Test for {@link PagingValidator#isValid(Integer, ConstraintValidatorContext)}.
+	 */
+	@Test
+	public void testIsValid_ok() {
+		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+			Validator validator = factory.getValidator();
+
+			PagingWrapper wrapper = new PagingWrapper(1);
+
+			Set<ConstraintViolation<PagingWrapper>> violations = validator.validate(wrapper);
+
+			assertEquals(0, violations.size());
+		}
+	}
+
+
+	@Setter
+	@Getter
+	private static class PagingWrapper {
+
+		@ValidPaging private Integer paging;
+
+		public PagingWrapper(Integer paging) {
+			this.paging = paging;
+		}
+	}
+}
