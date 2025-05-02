b946b1512df5048abd6b4dd1213d0e0fa1605ea7
poulakise <evanspoulaki@gmail.com>
2025-05-03 01:11:30 +0300

    feat(UserProfileDto): new DTO: UserProfileDto

---
+++ user-management/src/main/java/gr/evansp/momento/dto/UserProfileDto.java
@@ -0,0 +1,32 @@
+package gr.evansp.momento.dto;
+
+import gr.evansp.momento.model.UserProfile;
+
+/**
+ * Data transfer object for {@link UserProfile}.
+ *
+ * @param firstName
+ * 		firstName
+ * @param lastName
+ * 		lastName
+ * @param profilePictureUrl
+ * 		profilePictureUrl
+ */
+public record UserProfileDto(
+    String id,
+    String firstName,
+    String lastName,
+    String profilePictureUrl,
+    Long followsCount,
+    Long followedByCount) {
+
+  public static UserProfileDto of(UserProfile profile) {
+    return new UserProfileDto(
+        profile.getId().toString(),
+        profile.getFirstName(),
+        profile.getLastName(),
+        profile.getProfilePictureUrl(),
+        profile.getFollowsCount(),
+        profile.getFollowedByCount());
+  }
+}
