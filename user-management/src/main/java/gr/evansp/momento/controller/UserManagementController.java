5f3f718e5013a3061ca072973ddcb03542c44f83
poulakise <evanspoulaki@gmail.com>
2025-05-03 01:55:51 +0300

    feat(UserManagementController): Initial concrete implementation of UserManagementController.

--- user-management/src/main/java/gr/evansp/momento/controller/UserManagementController.java
+++ user-management/src/main/java/gr/evansp/momento/controller/UserManagementController.java
@@ -1,51 +1,152 @@
 package gr.evansp.momento.controller;
 
-import java.util.Optional;
+import gr.evansp.momento.dto.UpdateUserProfileDto;
+import gr.evansp.momento.dto.UserFollowDto;
+import gr.evansp.momento.dto.UserProfileDto;
+import gr.evansp.momento.model.UserFollow;
+import gr.evansp.momento.model.UserProfile;
+import gr.evansp.momento.service.UserManagementService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users/v1/")
 public class UserManagementController {
 
+  /**
+   * {@link UserManagementService}.
+   */
+  private final UserManagementService service;
+
+  @Autowired
+  public UserManagementController(UserManagementService service) {
+    this.service = service;
+  }
+
   @PostMapping("register")
-  public ResponseEntity<?> registerUser() {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<UserProfileDto> registerUser(
+      @RequestHeader("Authorization") String authorizationHeader) {
+
+    String jwtToken = extractJwtToken(authorizationHeader);
+    UserProfile profile = service.register(jwtToken);
+
+    return new ResponseEntity<>(UserProfileDto.of(profile), HttpStatus.OK);
   }
 
   @GetMapping("")
-  public ResponseEntity<?> getMe(@PathVariable String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<UserProfileDto> getLoggedInUser(
+      @RequestHeader("Authorization") String authorizationHeader) {
+
+    String jwtToken = extractJwtToken(authorizationHeader);
+    UserProfile profile = service.getLoggedInUser(jwtToken);
+
+    return new ResponseEntity<>(UserProfileDto.of(profile), HttpStatus.OK);
+  }
+
+  @GetMapping("followers")
+  public ResponseEntity<List<UserProfileDto>> getLoggedInUserFollowers(
+          @RequestHeader("Authorization") String authorizationHeader,
+          @RequestParam(defaultValue = "0") int page,
+          @RequestParam(defaultValue = "10") int size) {
+    String jwtToken = extractJwtToken(authorizationHeader);
+    List<UserFollow> follows = service.getLoggedInUserFollowedBy(jwtToken, page, size);
+
+    List<UserProfileDto> followers =
+            follows.stream().map(UserFollow::getFollowedBy).map(UserProfileDto::of).toList();
+
+    return new ResponseEntity<>(followers, HttpStatus.OK);
+  }
+
+  @GetMapping("follows")
+  public ResponseEntity<List<UserProfileDto>> getLoggedInUserFollows(
+          @RequestHeader("Authorization") String authorizationHeader,
+          @RequestParam(defaultValue = "0") int page,
+          @RequestParam(defaultValue = "10") int size) {
+    String jwtToken = extractJwtToken(authorizationHeader);
+    List<UserFollow> follows = service.getLoggedInUserFollows(jwtToken, page, size);
+
+    List<UserProfileDto> followers =
+            follows.stream().map(UserFollow::getFollowedBy).map(UserProfileDto::of).toList();
+
+    return new ResponseEntity<>(followers, HttpStatus.OK);
+  }
+
+  @PutMapping("")
+  public ResponseEntity<UserProfileDto> updateUser(
+      @RequestBody UpdateUserProfileDto userProfileDto,
+      @RequestHeader("Authorization") String authorizationHeader) {
+    String jwtToken = extractJwtToken(authorizationHeader);
+    UserProfile profile = service.updateProfile(jwtToken, userProfileDto);
+
+    return new ResponseEntity<>(UserProfileDto.of(profile), HttpStatus.OK);
   }
 
   @GetMapping("{userId}")
-  public ResponseEntity<?> getUser(@PathVariable String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<UserProfileDto> getUser(@PathVariable String userId) {
+    UserProfile profile = service.getUser(userId);
+
+    return new ResponseEntity<>(UserProfileDto.of(profile), HttpStatus.OK);
   }
 
   @GetMapping("{userId}/followers")
-  public ResponseEntity<?> getFollowers(@PathVariable String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<List<UserProfileDto>> getFollowers(
+      @PathVariable String userId,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    List<UserFollow> follows = service.getFollowedBy(userId, page, size);
+
+    List<UserProfileDto> followers =
+        follows.stream().map(UserFollow::getFollowedBy).map(UserProfileDto::of).toList();
+
+    return new ResponseEntity<>(followers, HttpStatus.OK);
   }
 
-  @PutMapping("{userId}")
-  public ResponseEntity<?> updateUser(@RequestBody String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  @GetMapping("{userId}/follows")
+  public ResponseEntity<List<UserProfileDto>> getFollows(
+      @PathVariable String userId,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    List<UserFollow> follows = service.getFollows(userId, page, size);
+
+    List<UserProfileDto> followsDto =
+        follows.stream().map(UserFollow::getFollowedBy).map(UserProfileDto::of).toList();
+
+    return new ResponseEntity<>(followsDto, HttpStatus.OK);
   }
 
   @PostMapping("{userId}/follow")
-  public ResponseEntity<?> follow(@PathVariable String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<UserFollowDto> follow(
+      @PathVariable String userId, @RequestHeader("Authorization") String authorizationHeader) {
+    String jwtToken = extractJwtToken(authorizationHeader);
+    UserFollow follow = service.follow(jwtToken, userId);
+
+    return new ResponseEntity<>(UserFollowDto.of(follow), HttpStatus.OK);
   }
 
   @PostMapping("{userId}/unfollow")
-  public ResponseEntity<?> unfollow(@PathVariable String userId) {
-    return ResponseEntity.of(Optional.of(""));
+  public ResponseEntity<Void> unfollow(
+      @PathVariable String userId, @RequestHeader("Authorization") String authorizationHeader) {
+    String jwtToken = extractJwtToken(authorizationHeader);
+    service.unfollow(jwtToken, userId);
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  private String extractJwtToken(String authorizationHeader) {
+    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      return authorizationHeader.substring(7);
+    }
+    return null;
   }
 }
