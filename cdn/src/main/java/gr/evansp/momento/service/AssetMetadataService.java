2ab30ee4c7b354cf249facb48f47c74d83743634
poulakise <evanspoulaki@gmail.com>
2025-06-25 11:48:53 +0300

    feat(AssetMetadataService): new service, AssetMetadataService.

---
+++ cdn/src/main/java/gr/evansp/momento/service/AssetMetadataService.java
@@ -0,0 +1,20 @@
+package gr.evansp.momento.service;
+
+import gr.evansp.momento.model.Asset;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * {@link Service} to manage {@link Asset} Metadata.
+ */
+public interface AssetMetadataService {
+
+	/**
+	 * Stores {@link Asset} metadata
+	 * @param file file
+	 * @param storedFilename storedFilename
+	 * @param contentHash contentHash
+	 * @return {@link Asset}
+	 */
+	Asset storeAssetMetadata(MultipartFile file, String storedFilename, String contentHash);
+}
