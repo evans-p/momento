b495c38f30713e98ca7ae957bedb05441bd6d09e
poulakise <evanspoulaki@gmail.com>
2025-06-25 11:56:33 +0300

    feat(UserManagementServiceImpl): cleanup.

--- user-management/src/main/java/gr/evansp/momento/service/UserManagementServiceImpl.java
+++ user-management/src/main/java/gr/evansp/momento/service/UserManagementServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
