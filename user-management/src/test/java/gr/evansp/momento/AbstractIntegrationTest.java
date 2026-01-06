a7e3c54404ee23bcc89074ba4b69e086224feadc
poulakise <evanspoulaki@gmail.com>
2026-01-06 03:27:31 +0200

    feat(AbstractIntegrationTest): refactor

--- user-management/src/test/java/gr/evansp/momento/AbstractIntegrationTest.java
+++ user-management/src/test/java/gr/evansp/momento/AbstractIntegrationTest.java
@@ -2,7 +2,9 @@ package gr.evansp.momento;
 
 import gr.evansp.momento.repository.UserFollowRepository;
 import gr.evansp.momento.repository.UserProfileRepository;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -16,30 +18,36 @@ import org.testcontainers.containers.wait.strategy.Wait;
  */
 public abstract class AbstractIntegrationTest extends AbstractUnitTest {
 
-  private static final PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:14")
-          .withDatabaseName("user-management")
-          .withUsername("postgres")
-          .withPassword("postgres")
-          .withExposedPorts(5432)
-          .withInitScript("sql/ddl.sql")
-          .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 2));
-
-  static {
-    postgres.start();
-  }
+  private static PostgreSQLContainer<?> postgres;
 
   @Autowired UserProfileRepository userProfileRepository;
 
   @Autowired UserFollowRepository userFollowRepository;
+
+  @BeforeAll
+  public static void setup() {
+    postgres =
+            new PostgreSQLContainer<>("postgres:14")
+                    .withDatabaseName("user-management")
+                    .withUsername("postgres")
+                    .withPassword("postgres")
+                    .withExposedPorts(5432)
+                    .withInitScript("sql/ddl.sql")
+                    .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 2));
+
+    postgres.start();
+  }
+
+  @AfterAll
+  public static void shutdown() {
+    postgres.stop();
+  }
 
   @DynamicPropertySource
   private static void registerPgProperties(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", postgres::getJdbcUrl);
     registry.add("spring.datasource.username", postgres::getUsername);
     registry.add("spring.datasource.password", postgres::getPassword);
-    registry.add(
-        "spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
   }
 
   @BeforeEach
