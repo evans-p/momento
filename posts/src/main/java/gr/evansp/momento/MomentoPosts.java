4d0cc284206872022880f05c7a7e1f650f6e94f8
poulakise <evanspoulaki@gmail.com>
2026-01-10 04:16:43 +0200

    feat(posts): new module, posts

---
+++ posts/src/main/java/gr/evansp/momento/MomentoPosts.java
@@ -0,0 +1,11 @@
+package gr.evansp.momento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentoPosts {
+  public static void main(String[] args) {
+    SpringApplication.run(MomentoPosts.class, args);
+  }
+}
